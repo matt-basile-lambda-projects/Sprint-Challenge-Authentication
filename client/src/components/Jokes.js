072300a23ca307f9b3ceabf3fe14cc52182bb261
Matt Basile <matthewbasile2@gmail.com>
2019-03-01 12:15:36 -0500

    Complete Stretch objectives of styling out a frontend.

--- client/src/components/Jokes.js
+++ client/src/components/Jokes.js
@@ -1,11 +1,37 @@
 import React, { Component } from 'react'
+import axios from 'axios'
+import requiresAuth from '../auth/requiresAuth'
 
-export default class Jokes extends Component {
+class Jokes extends Component {
+  state = {
+    jokes: [],
+  };
+  componentDidMount() {
+    const endpoint = 'http://localhost:3300/api/jokes';
+    axios.get(endpoint).then(res => {
+      console.log(res)
+      this.setState({ jokes: res.data });
+    });
+  }
+  logout = () => {
+    localStorage.removeItem('jwt');
+
+    this.props.history.push('/');
+  };
   render() {
+    console.log(this.state.jokes.joke)
     return (
       <div>
-        <h1>Jokes</h1>
+        <h2>Wanna hear a joke??</h2>
+          <ul>
+            {this.state.jokes.map(joke => (
+              <li key={joke.id}>{joke.joke}</li>
+            ))}
+          </ul>
+          <button onClick={this.logout}>Logout</button>
       </div>
     )
   }
 }
+
+export default requiresAuth(Jokes)
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -62,8 +62,8 @@ export default class Login extends Component {
                 placeholder="**********"
                 />
             </div>
-            <div className="flex items-center justify-between">
-                <button className="bg-green hover:bg-green-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
+            <div className="flex w-full items-center justify-around">
+                <button className="bg-blue hover:bg-blue-dark text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="submit">
                 Log In
                 </button>
                 <Link to="/register">
