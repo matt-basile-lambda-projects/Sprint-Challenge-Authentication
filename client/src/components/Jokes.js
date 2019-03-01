6c6f948aa56f43749d308669872ea91674984095
Matt Basile <matthewbasile2@gmail.com>
2019-03-01 13:05:13 -0500

    Styled up jokes to form cards with a joke and punchline side. Jokes that were unable to be split properly were discarded.

--- client/src/components/Jokes.js
+++ client/src/components/Jokes.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import requiresAuth from '../auth/requiresAuth'
-
+import JokeCard from './JokeCard'
 class Jokes extends Component {
   state = {
     jokes: [],
@@ -22,13 +22,20 @@ class Jokes extends Component {
     console.log(this.state.jokes.joke)
     return (
       <div>
-        <h2>Wanna hear a joke??</h2>
-          <ul>
-            {this.state.jokes.map(joke => (
-              <li key={joke.id}>{joke.joke}</li>
+        <div className="mt-8 flex justify-between w-4/5 mx-auto">
+          <h2 className="text-center">Wanna hear a joke??</h2>
+          <button className ="bg-red hover:bg-red-dark px-4 py-2 text-white rounded w-1/6" onClick={this.logout}>Logout</button>
+        </div>
+        
+          <div className="w-full flex flex-wrap justify-center">
+            {this.state.jokes.map((joke, i) => (
+              <JokeCard id={i} joke={joke.joke} key={joke.id}/>
             ))}
-          </ul>
-          <button onClick={this.logout}>Logout</button>
+
+
+
+          </div>
+         
       </div>
     )
   }
